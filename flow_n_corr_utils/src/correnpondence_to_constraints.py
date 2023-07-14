c7fa6d408187c8d3df27ef357dd7b909b9a4df25
shahar.zuler <shaharzuler@gmail.com>
2023-07-14 22:46:13 +0300

    added interpolation of vals close to the (sparse) constraints in order to thicken the constraints

--- flow_n_corr_utils/src/correnpondence_to_constraints.py
+++ flow_n_corr_utils/src/correnpondence_to_constraints.py
@@ -1,6 +1,8 @@
 from typing import Tuple
 
 import numpy as np
+from scipy.interpolate import griddata
+from scipy.spatial import cKDTree
 
 from three_d_data_manager import save_arr
 
@@ -13,17 +15,37 @@ class Corr2ConstraintsConvertor:
     def __init__(self) -> None:
         pass
 
-    def convert_corr_to_constraints(self, correspondence_h5_path:str, k_nn:int, output_folder_path:str, output_constraints_shape:Tuple) -> str:
+    def convert_corr_to_constraints(self, correspondence_h5_path:str, k_nn:int, output_folder_path:str, output_constraints_shape:Tuple, k_interpolate_sparse_constraints_nn:int=124) -> str:
         template_point_cloud, unlabeled_point_cloud, correspondence_template_unlabeled = get_point_clouds_and_corr_from_h5(correspondence_h5_path)
-        flow_template_unlabeled = self.flow_from_corr(template_point_cloud, unlabeled_point_cloud, correspondence_template_unlabeled)
+        flow_template_unlabeled = self._flow_from_corr(template_point_cloud, unlabeled_point_cloud, correspondence_template_unlabeled)
         smooth_flow_template_unlabeled = smooth_flow(template_point_cloud, flow_template_unlabeled, k_nn)
         voxelized_flow = voxelize_flow(smooth_flow_template_unlabeled, template_point_cloud, output_constraints_shape) 
-        output_file_path = save_arr(output_folder_path, "constraints", voxelized_flow)
 
+        if k_interpolate_sparse_constraints_nn>1:
+            for axis in range(voxelized_flow.shape[-1]):
+                self._interpolate_knn_axis(k_interpolate_sparse_constraints_nn, voxelized_flow, axis)
+
+        output_file_path = save_arr(output_folder_path, "constraints", voxelized_flow)
         return output_file_path
 
+    def _interpolate_knn_axis(self, k_interpolate_sparse_constraints_nn:int, voxelized_flow:np.array, axis:int) -> np.array:
+        data_mask = np.isfinite(voxelized_flow[:,:,:,axis] )
+        data_coords = np.array(np.where(data_mask)).T
+        data_values = voxelized_flow[:,:,:,axis][data_mask]
+
+        nan_mask = np.isnan(voxelized_flow[:,:,:,axis] )
+        nan_coords = np.array(np.where(nan_mask)).T
+
+        kdtree = cKDTree(nan_coords)
+        distances, nn_indices = kdtree.query(data_coords, k=k_interpolate_sparse_constraints_nn)
+        nan_coords_for_interp = nan_coords[nn_indices].reshape(-1,3)
+
+        interpolated_values = griddata(data_coords, data_values, nan_coords_for_interp , method='linear')
+        voxelized_flow[nan_coords_for_interp[:,0], nan_coords_for_interp[:,1], nan_coords_for_interp[:,2], axis ] = interpolated_values
+        return voxelized_flow
+
     @staticmethod
-    def flow_from_corr(point_cloud1:np.array, point_cloud2:np.array, correspondence12:np.array) -> np.array:
+    def _flow_from_corr(point_cloud1:np.array, point_cloud2:np.array, correspondence12:np.array) -> np.array:
         point_cloud2_in_point_cloud1_coords = point_cloud2[correspondence12]
         flow12 = point_cloud2_in_point_cloud1_coords - point_cloud1
         return flow12
--- run.py
+++ run.py
@@ -2,10 +2,11 @@ from flow_n_corr_utils import Corr2ConstraintsConvertor
 
 
 output_constraints_arr_path = Corr2ConstraintsConvertor().convert_corr_to_constraints(
-    correspondence_h5_path="/home/shahar/cardio_corr/my_packages/sts_project/STS/outputs_20230712_121917/inference_20230712_121934/model_inference.hdf5",
+    correspondence_h5_path="/home/shahar/cardio_corr/outputs/outputs_20230714_005246/sts_training_output_20230714_005849/inference_20230714_005909/model_inference.hdf5",
      k_nn=10, 
      output_folder_path="/home/shahar/cardio_corr/my_packages/flow_n_corr_utils_project/flow_n_corr_utils/outputs",
-     output_constraints_shape=(200, 200, 136, 3)
+     output_constraints_shape=(200, 200, 136, 3),
+     k_interpolate_sparse_constraints_nn=124 #a 5x5x5 pixels cube
      )
 
 
