dac07627c641b8745a30575e3c6c10e9a1048894
shahar.zuler <shaharzuler@gmail.com>
2023-08-29 16:09:50 +0300

    in flow2corr fixed knn usage, direction and explanations

--- flow_n_corr_utils/src/flow_to_correspondence.py
+++ flow_n_corr_utils/src/flow_to_correspondence.py
@@ -1,5 +1,7 @@
 import numpy as np
-from sklearn.neighbors import NearestNeighbors
+import torch
+
+from .utils.geometry_utils import knn
 
 def flow_to_corr(flow:np.ndarray, target_pc:np.ndarray, source_pc:np.ndarray) -> np.ndarray: # shapes [x,y,z,3], [N, 3], [N, 3]
     """
@@ -7,17 +9,15 @@ def flow_to_corr(flow:np.ndarray, target_pc:np.ndarray, source_pc:np.ndarray) ->
     flow.shape = [x,y,z,3]
     target_pc.shape = [N, 3]
     source_pc.shape = [N, 3]
-    The returned indices are the indices of the source_pc corresponding to target_pc
+    The returned indices are the indices of the target_pc corresponding to source_pc, meaning: 
+    source_estimated_coords[indices] =~ source_pc
+    target_pc[indices] corresponds to source_pc
     """
-    source_as_int = np.round(source_pc).astype(int)
-    flow_in_source_coords = flow[source_as_int[:,0], source_as_int[:,1], source_as_int[:,2], :]
-    target_estimated_coords = source_as_int + flow_in_source_coords
+    target_as_int = np.round(target_pc).astype(int)
+    flow_in_target_coords = flow[target_as_int[:,0], target_as_int[:,1], target_as_int[:,2], :]
+    source_estimated_coords = target_as_int + flow_in_target_coords
+        
+    indices = knn(torch.tensor(source_estimated_coords), torch.tensor(source_pc), 1)[1]
     
-    nbrs = NearestNeighbors(n_neighbors=1, algorithm='ball_tree').fit(target_estimated_coords)
-    distances, indices = nbrs.kneighbors(target_pc)
-
-    # now target_estimated_coords[indices][:,0,:] =~ target_pc
-    # now source_pc[indices][:,0,:] corresponds to target_pc
-
-    return indices[:,0]
+    return indices
 
